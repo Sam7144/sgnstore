eed42e9cb39bdeba9d5198aa98e99dea4b1da670
Sam7144 <79979572+Sam7144@users.noreply.github.com>
2023-04-15 05:54:48 +0300

    remove store

--- pages/Store/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CartSlice from "./CartSlice.js";
-const Store=configureStore({
-    reducer:{
-        cart:CartSlice
-    }
-})
-export default Store;
